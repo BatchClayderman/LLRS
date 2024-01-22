bacecc0e791bea7b250f6333b3d2ee4b29aec35e
Batch Clayderman <57588843+BatchClayderman@users.noreply.github.com>
2024-01-22 17:49:29 +0800

    Add files via upload

--- LLRS_v2.0.py
+++ LLRS_v2.0.py
@@ -1,4 +1,4 @@
-from sys import exit
+from sys import argv, exit
 from copy import deepcopy
 from datetime import datetime
 from math import log2
@@ -17,20 +17,24 @@ except Exception as e:
 	exit(-1)
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
+EOF = (-1)
+SCRIPT_NAME = "LLRS_v2.0.py"
 DEFAULT_Q = 256
 DEFAULT_N = 256
 DEFAULT_M = 4096
 DEFAULT_D = 10
 DEFAULT_K = 4
-Ns = [2, 4, 6, 8, 16, 32] # only modify the parameters here
+parameters_dict = {} # {"q":16, "n":16, "m":256}
+Ns = [2, 4, 6, 8, 16, 32] # Users should only modify the parameters here in this line and the previous line to accomplish more experiments. 
 
 
+# Class #
 class PARS:
-	def __init__(self, N = 2, q = DEFAULT_Q, n = DEFAULT_N, m = DEFAULT_M, d = DEFAULT_D, k = DEFAULT_K, isAvailable = False):
+	def __init__(self, N = 2, q = DEFAULT_Q, n = DEFAULT_N, m = DEFAULT_M, d = DEFAULT_D, k = DEFAULT_K, isAvailable = False, **extra_pars):
 		if isinstance(N, int) and N > 1:
 			self.__N = N
 		else:
-			print("The input n is not a positive integer that is larger than 1. It is defaulted to 2. ")
+			print("The input N is not a positive integer that is larger than 1. It is defaulted to 2. ")
 			self.__N = 2
 		if isinstance(q, int) and q > 2 and log2(q) == int(log2(q)):
 			self.__q = q
@@ -42,24 +46,32 @@ class PARS:
 		else:
 			print("The input n is not a positive integer. It is defaulted to {0}. ".format(DEFAULT_N))
 			self.__n = DEFAULT_N
-		if isinstance(m, int) and m >= (self.__n << 1) * log2(self.__q):
+		if isinstance(m, int) and m > 0:
 			self.__m = m
 		else:
-			print("The input m is not a positive integer or it does not meet the requirement that \"m >= 2n lb q\". It is defaulted to {0}. ".format(DEFAULT_M))
+			print("The input m is not a positive integer. It is defaulted to {0}. ".format(DEFAULT_M))
 			self.__m = DEFAULT_M
 		if isinstance(d, int) and d > 0:
 			self.__d = d
 		else:
 			print("The input d is not a positive integer. It is defaulted to {0}. ".format(DEFAULT_D))
 			self.__d = DEFAULT_D
-		if isinstance(k, int) and k > 0 and self.__m % (k << 1) == 0:
+		if isinstance(k, int) and k > 0:
 			self.__k = k
 		else:
-			print("The input k is not a positive integer or it does not meet the requirement that \"2k | m\". It is defaulted to {0}. ".format(DEFAULT_K))
+			print("The input k is not a positive integer. It is defaulted to {0}. ".format(DEFAULT_K))
+			self.__k = DEFAULT_K
+		if self.__m < (self.__n << 1) * log2(self.__q) or self.__m % (self.__n << 2) != 0 or self.__m % (self.__k << 1) != 0:
+			print("The input q, n, m, and k should meet the requirements that \"m >= 2n lb q\", \"4n | m\", and \"2k | m\". Nonetheless, one or more of the requirements are not satisfied. They are defaulted to {0}, {1}, {2} and {3} respectively. ".format(DEFAULT_Q, DEFAULT_N, DEFAULT_M, DEFAULT_K))
+			self.__q = DEFAULT_Q
+			self.__n = DEFAULT_N
+			self.__m = DEFAULT_M
 			self.__k = DEFAULT_K
 		self.isAvailable = isAvailable if isinstance(isAvailable, bool) else False
 		self.__sk = zeros((0, self.__m, self.__m), dtype = "int")
 		self.__pk = zeros((0, self.__n, self.__m), dtype = "int")
+		if extra_pars:
+			print("Extra parameters for setting up are detected, listed as follows. \n{0}\n\n*** Please check the global parameter dictionary. ***\n".format(list(extra_pars.keys())))
 	def getUpperN(self) -> int:
 		return self.__N
 	def getQ(self) -> int:
@@ -136,7 +148,7 @@ def TrapGen(pars:PARS) -> tuple:
 	B = randint(q, size = (n, m >> 2)) # size = (n, m / 4)
 	R = randint(2, size = (m >> 2, m >> 2)) # size = (m / 4, m / 4)
 	A_0i = concatenate((B, (dot(B, R) % q + G) % q), axis = 1) # size = (n, m / 2)
-	T_g = zeros((m >> 2, m >> 2), dtype = "int") # size = (m / 4, m / 4)
+	T_g = zeros((m // (n << 2), m // (n << 2)), dtype = "int") # size = (m / 4n, m / 4n)
 	fill_diagonal(T_g, 2)
 	fill_diagonal(T_g[1:], -1)
 	T_G = kron(eye(n, dtype = "int"), T_g) % q # size = (m / 4, m / 4)
@@ -167,32 +179,12 @@ def GenSample(pars:PARS, C_miu) -> array:
 	z %= q
 	return e
 
-def getCurrentTime() -> str:
-	tz = timezone("Asia/Shanghai")
-	current_time = datetime.now(tz)
-	return "{0} {1}".format(current_time.strftime("%Y/%m/%d %H:%M:%S"), current_time.tzname())
 
-def preExit(countdownTime = 5) -> None:
-	try:
-		cntTime = int(countdownTime)
-		length = len(str(cntTime))
-	except:
-		return
-	print()
-	while cntTime > 0:
-		print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(cntTime), end = "")
-		try:
-			sleep(1)
-		except:
-			print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(0))
-			return
-		cntTime -= 1
-	print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(cntTime))
-
-
-# Main Functions #
-def Setup(N) -> PARS:
-	pars = PARS(N)
+# Procedure Functions #
+def Setup(N:int, pars_dict:dict) -> PARS:
+	_pars_dict = pars_dict.copy()
+	_pars_dict["N"] = N
+	pars = PARS(**_pars_dict)
 	q = pars.getQ()
 	n = pars.getLowerN()
 	m = pars.getM()
@@ -286,7 +278,7 @@ def KeyUpdate(pars:PARS) -> tuple:
 		(T_111, )			\
 	)
 	t = randint(7)
-	print(sk[t])
+	print("sk_t = {0}".format(sk[t]))
 	end_time = time()
 	return (pars, end_time - start_time)
 	
@@ -353,60 +345,185 @@ def Link(pars1:PARS, pars2:PARS) -> bool:
 		print("unlink")
 		return False
 
+
+# Main Functions #
+def getCurrentTime() -> str:
+	tz = timezone("Asia/Shanghai")
+	current_time = datetime.now(tz)
+	return "{0} {1}".format(current_time.strftime("%Y/%m/%d %H:%M:%S"), current_time.tzname())
+
+def printHelp() -> None:
+	print("\"{0}\": A Python script for implementing LLRS, which is optimized by improving the TrapGen child procedure. ".format(SCRIPT_NAME), end = "\n\n")
+	print("Option: ")
+	print("\t[/q|-q|q]: Specify that the following option is the value of q (default: {0}). ".format(DEFAULT_Q))
+	print("\t[/n|-n|n]: Specify that the following option is the value of n (default: {0}). ".format(DEFAULT_N))
+	print("\t[/m|-m|m]: Specify that the following option is the value of m (default: {0}). ".format(DEFAULT_M))
+	print("\t[/d|-d|d]: Specify that the following option is the value of d (default: {0}). ".format(DEFAULT_D))
+	print("\t[/k|-k|k]: Specify that the following option is the value of k (default: {0}). ".format(DEFAULT_K))
+	print("\t[/N|-N|N]: Specify that the following options are the values of N. ", end = "\n\n")
+	print("Format: ")
+	print("\tpython \"{0}\" [/q|-q|q] q [/n|-n|n] n [/m|-m|m] m [/d|-d|d] d [/k|-k|k] k [/N|-N|N] N1 N2 ...".format(SCRIPT_NAME))
+	print("\tpython \"{0}\" [/h|-h|h|/help|--help|help]".format(SCRIPT_NAME), end = "\n\n")
+	print("Example: ")
+	print("\tpython \"{0}\"".format(SCRIPT_NAME))
+	print("\tpython \"{0}\" /q {1} /n {2} /m {3}".format(SCRIPT_NAME, DEFAULT_Q, DEFAULT_N, DEFAULT_M))
+	print("\tpython \"{0}\" -q {1} -n {2} -m {3} -d {4} -k {5}".format(SCRIPT_NAME, DEFAULT_Q, DEFAULT_N, DEFAULT_M, DEFAULT_D, DEFAULT_K))
+	print("\tpython \"{0}\" [/q|-q|q] q [/n|-n|n] n [/m|-m|m] m [/d|-d|d] d [/k|-k|k] k [/N|-N|N] N1 N2 ...".format(SCRIPT_NAME))
+	print("\tpython \"{0}\" [/h|-h|h|/help|--help|help]".format(SCRIPT_NAME), end = "\n\n")
+	print("Exit code: ")
+	print("\t{0}\tThe Python script finished successfully. ".format(EXIT_SUCCESS))
+	print("\t{0}\tThe Python script finished not passing all the verifications. ".format(EXIT_FAILURE))
+	print("\t{0}\tThe Python script received unrecognized commandline options. ".format(EOF), end = "\n\n")
+	print("Note: ")
+	print("\t1) All the commandline options are case-sensitive (except \"[/h|-h|h|/help|--help|help]\") and optional. ")
+	print("\t2) The parameters q, n, m, d, and k should be positive integers and will obey the following priority: values obtained from the commandline > values specified by the user within the script > default values set within the script. ")
+	print("\t3) The values of N specified from the commandline will be directly appended to those specified by the user within the script. Each value of N should be an integer that is larger than 1. The unsatisfying ones will be set to 2. ")
+	print("\t4) The value of q should be a 2-based integer that is larger than 2. Otherwise, it will be defaulted to {0}. ".format(DEFAULT_Q))
+	print("\t5) The parameters q, n, m, and k should meet the requirements that \"m >= 2n lb q\", \"4n | m\", and \"2k | m\". If one or more of the requirements are not satisfied, they will be set to their default values respectively. ", end = "\n\n")
+
+def handleCommandline() -> dict:
+	for arg in argv[1:]:
+		if arg.lower() in ("/h", "-h", "h", "/help", "--help", "help", "/?", "-?", "?"):
+			printHelp()
+			return True
+	commandline_dict = {}
+	pointer = None
+	for arg in argv[1:]:
+		if arg in ("/q", "-q", "q"):
+			pointer = "q"
+		elif arg in ("/n", "-n", "n"):
+			pointer = "n"
+		elif arg in ("/m", "-m", "m"):
+			pointer = "m"
+		elif arg in ("/d", "-d", "d"):
+			pointer = "d"
+		elif arg in ("/k", "-k", "k"):
+			pointer = "k"
+		elif arg in ("/N", "-N", "N"):
+			pointer = "N"
+		elif pointer is None:
+			print("Error handling commandline, please check your commandline or use \"/help\" for help. ")
+			return False
+		elif "N" == pointer:
+			commandline_dict.setdefault("N", [])
+			commandline_dict["N"].append(arg)
+		else:
+			commandline_dict[pointer] = arg
+			pointer = None # reset
+	for key in ("q", "n", "m", "d", "k"):
+		try:
+			if key in commandline_dict:
+				commandline_dict[key] = int(commandline_dict[key])
+		except:
+			print("Error regarding {0} as an integer. Please check your commandline. ".format(key))
+			return False
+	try:
+		if "N" in commandline_dict:
+			commandline_dict["N"] = [int(item) for item in commandline_dict["N"]]
+	except:
+		print("Error regarding all the values in N as integers. Please check your commandline. ".format(key))
+		return False
+	return commandline_dict
+
+def preExit(countdownTime = 5) -> None:
+	try:
+		cntTime = int(countdownTime)
+		length = len(str(cntTime))
+	except:
+		return
+	print()
+	while cntTime > 0:
+		print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(cntTime), end = "")
+		try:
+			sleep(1)
+		except:
+			print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(0))
+			return
+		cntTime -= 1
+	print("\rProgram ended, exiting in {{0:>{0}}} second(s). ".format(length).format(cntTime))
+
 def main() -> int:
-	print("Program named \"LLRS_v2.0.py\" started at {0}. ".format(getCurrentTime()), end = "\n" * 3)
+	if not argv[0].endswith(SCRIPT_NAME):
+		print("Warning: This Python script should be named \"{0}\". However, it is currently specified as another name. ".format(SCRIPT_NAME))
+	commandlineArgument = handleCommandline()
+	if isinstance(commandlineArgument, bool):
+		return EXIT_SUCCESS if commandlineArgument else EOF
+	print("Program named \"{0}\" started at {1}. ".format(SCRIPT_NAME, getCurrentTime()))
+	if commandlineArgument:
+		print("Parameters resolved from commandline: {0}".format(commandlineArgument))
+		if "N" in commandlineArgument:
+			Ns.extend(commandlineArgument["N"])
+			del commandlineArgument["N"]
+		parameters_dict.update(commandlineArgument)
+	print("Parameters: {0}".format(parameters_dict))
+	print("Ns: {0}".format(Ns), end = "\n" * 3)
 	bRet = True
 	dicts = {}
 	for N in Ns:
-		print("/** N = {0} **/".format(N))
-		
-		print("/* Setup */")
-		start_time = time()
-		pars = Setup(N)
-		end_time = time()
-		dicts.setdefault(N, {"Setup":end_time - start_time})
-		print()
-		
-		print("/* KeyExtract */")
-		totalTimeCost = 0
-		for i in range(N):
-			pars, timeCost = KeyExtract(pars)
-			totalTimeCost += timeCost
-		pars.printVars(["sk", "pk"])
-		dicts[N]["KeyExtract"] = totalTimeCost
-		print()
-		
-		print("/* KeyUpdate */")
-		pars, dicts[N]["KeyUpdate"] = KeyUpdate(pars)
-		pars1 = deepcopy(pars)
-		pars2 = deepcopy(pars)
-		print()
-		
-		print("/* Sign1 */")
-		pars1, dicts[N]["Sign1"] = Sign(pars1)
-		print()
-		
-		print("/* Verify1 */")
-		status, dicts[N]["Verify1"] = Verify(pars1)
-		bRet = bRet and status
-		print()
-		
-		print("/* Sign2 */")
-		pars2, dicts[N]["Sign2"] = Sign(pars2)
-		print()
-		
-		print("/* Verify2 */")
-		status, dicts[N]["Verify2"] = Verify(pars2)
-		bRet = bRet and status
-		print()
-		
-		print("/* Link */")
-		start_time = time()
-		Link(pars1, pars2)
-		end_time = time()
-		dicts[N]["Link"] = end_time - start_time
-		print()
-	print(dicts)
+		if isinstance(N, int) and N >= 2:
+			print("/** N = {0} **/".format(N))
+			
+			print("/* Setup */")
+			start_time = time()
+			pars = Setup(N, parameters_dict)
+			end_time = time()
+			dicts.setdefault(N, {"Setup":end_time - start_time})
+			print()
+			
+			print("/* KeyExtract */")
+			totalTimeCost = 0
+			for i in range(N):
+				pars, timeCost = KeyExtract(pars)
+				totalTimeCost += timeCost
+			pars.printVars(["sk", "pk"])
+			dicts[N]["KeyExtract"] = totalTimeCost
+			print()
+			
+			print("/* KeyUpdate */")
+			pars, dicts[N]["KeyUpdate"] = KeyUpdate(pars)
+			pars1 = deepcopy(pars)
+			pars2 = deepcopy(pars)
+			print()
+			
+			print("/* Sign1 */")
+			pars1, dicts[N]["Sign1"] = Sign(pars1)
+			print()
+			
+			print("/* Verify1 */")
+			status, dicts[N]["Verify1"] = Verify(pars1)
+			bRet = bRet and status
+			print()
+			
+			print("/* Sign2 */")
+			pars2, dicts[N]["Sign2"] = Sign(pars2)
+			print()
+			
+			print("/* Verify2 */")
+			status, dicts[N]["Verify2"] = Verify(pars2)
+			bRet = bRet and status
+			print()
+			
+			print("/* Link */")
+			start_time = time()
+			Link(pars1, pars2)
+			end_time = time()
+			dicts[N]["Link"] = end_time - start_time
+			print()
+		else:
+			skipped_N.append(N)
+	if skipped_N:
+		if 1 == len(skipped_N):
+			print("The following value of \"N\" has been skipped as it is not an integer that is larger than 2. \n{0}\n".format(skipped_N))
+		else:
+			print("The following values of \"N\" have been skipped as they are not integers that are larger than 2. \n{0}\n".format(skipped_N))
+	if 0 == len(dicts):
+		print("No experimental results are collected. ")
+	elif 1 == len(dicts):
+		print("The experimental result of the time consumption in seconds is shown as follows. ")
+		print(dicts)
+	else:
+		print("The experimental results of the time consumption in seconds are shown as follows. ")
+		print(dicts)
 	preExit()
 	print("\n\n\nProgram ended at {0} with exit code {1}. ".format(getCurrentTime(), EXIT_SUCCESS if bRet else EXIT_FAILURE))
 	return EXIT_SUCCESS if bRet else EXIT_FAILURE
---
+++ readme.md
@@ -0,0 +1,67 @@
+"LLRS_v1.0.py": A Python script for implementing LLRS, which will solve integer nullspace (Ae = O (mod q)) directly. 
+
+Option: 
+	[/q|-q|q]: Specify that the following option is the value of q (default: 256). 
+	[/n|-n|n]: Specify that the following option is the value of n (default: 256). 
+	[/m|-m|m]: Specify that the following option is the value of m (default: 4096). 
+	[/d|-d|d]: Specify that the following option is the value of d (default: 10). 
+	[/k|-k|k]: Specify that the following option is the value of k (default: 4). 
+	[/N|-N|N]: Specify that the following options are the values of N. 
+	[/h|-h|h|/help|--help|help]: Show this help information. This option is not case-sensitive. 
+
+Format: 
+	python "LLRS_v1.0.py" [/q|-q|q] q [/n|-n|n] n [/m|-m|m] m [/d|-d|d] d [/k|-k|k] k [/N|-N|N] N1 N2 ...
+	python "LLRS_v1.0.py" [/h|-h|h|/help|--help|help]
+
+Example: 
+	python "LLRS_v1.0.py"
+	python "LLRS_v1.0.py" /q 256 /n 256 /m 4096
+	python "LLRS_v1.0.py" -q 256 -n 256 -m 4096 -d 10 -k 4
+	python "LLRS_v1.0.py" q 256 n 256 m 4096 d 10 k 4 N 2 4 8
+	python "LLRS_v1.0.py" --help
+
+Exit code: 
+	0	The Python script finished successfully. 
+	1	The Python script finished not passing all the verifications. 
+	-1	The Python script received unrecognized commandline options. 
+
+Note: 
+	1) All the commandline options are case-sensitive (except "[/h|-h|h|/help|--help|help]") and optional. 
+	2) The parameters q, n, m, d, and k should be positive integers and will obey the following priority: values obtained from the commandline > values specified by the user within the script > default values set within the script. 
+	3) The values of N specified from the commandline will be directly appended to those specified by the user within the script. Each value of N should be an integer that is larger than 1. The unsatisfying ones will be set to 2. 
+	4) The value of q should be a 2-based integer that is larger than 2. Otherwise, it will be defaulted to 256. 
+	5) The parameters q, n, m, and k should meet the requirements that "m >= 2n lb q", "4n | m", and "2k | m". If one or more of the requirements are not satisfied, they will be set to their default values respectively. 
+
+"LLRS_v2.0.py": A Python script for implementing LLRS, which is optimized by improving the TrapGen child procedure. 
+
+Option: 
+	[/q|-q|q]: Specify that the following option is the value of q (default: 256). 
+	[/n|-n|n]: Specify that the following option is the value of n (default: 256). 
+	[/m|-m|m]: Specify that the following option is the value of m (default: 4096). 
+	[/d|-d|d]: Specify that the following option is the value of d (default: 10). 
+	[/k|-k|k]: Specify that the following option is the value of k (default: 4). 
+	[/N|-N|N]: Specify that the following options are the values of N. 
+
+Format: 
+	python "LLRS_v2.0.py" [/q|-q|q] q [/n|-n|n] n [/m|-m|m] m [/d|-d|d] d [/k|-k|k] k [/N|-N|N] N1 N2 ...
+	python "LLRS_v2.0.py" [/h|-h|h|/help|--help|help]
+
+Example: 
+	python "LLRS_v2.0.py"
+	python "LLRS_v2.0.py" /q 256 /n 256 /m 4096
+	python "LLRS_v2.0.py" -q 256 -n 256 -m 4096 -d 10 -k 4
+	python "LLRS_v2.0.py" [/q|-q|q] q [/n|-n|n] n [/m|-m|m] m [/d|-d|d] d [/k|-k|k] k [/N|-N|N] N1 N2 ...
+	python "LLRS_v2.0.py" [/h|-h|h|/help|--help|help]
+
+Exit code: 
+	0	The Python script finished successfully. 
+	1	The Python script finished not passing all the verifications. 
+	-1	The Python script received unrecognized commandline options. 
+
+Note: 
+	1) All the commandline options are case-sensitive (except "[/h|-h|h|/help|--help|help]") and optional. 
+	2) The parameters q, n, m, d, and k should be positive integers and will obey the following priority: values obtained from the commandline > values specified by the user within the script > default values set within the script. 
+	3) The values of N specified from the commandline will be directly appended to those specified by the user within the script. Each value of N should be an integer that is larger than 1. The unsatisfying ones will be set to 2. 
+	4) The value of q should be a 2-based integer that is larger than 2. Otherwise, it will be defaulted to 256. 
+	5) The parameters q, n, m, and k should meet the requirements that "m >= 2n lb q", "4n | m", and "2k | m". If one or more of the requirements are not satisfied, they will be set to their default values respectively. 
+
